a056d81b5b7f810f81a58a53fe35dd7718f0fe30
Kitty <k.jenkins-phoenix2@newcastle.ac.uk>
2025-11-27 23:28:08 +0000

    Updated The Registration Form And Added The Change Password Functionality With Validation

--- Computer_Program_Starter/Computer_Program_Starter/app/forms.py
+++ Computer_Program_Starter/Computer_Program_Starter/app/forms.py
@@ -1,29 +1,46 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Regexp
 
 # Form For User Registration
 class RegistrationForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(min=6,max=45)])
+    usernameEmail = StringField("Username Email", validators=[DataRequired(), Email(), Length(min=6,max=45)])
     # Password Must Be Between 12 And 25 Characters Long, Contain At Least One Uppercase Letter, One Lowercase Letter, One Number, And One Special Character
-    password = PasswordField('Password', 
+    password = PasswordField("Password", 
                          validators=[
                              DataRequired(), 
                              Length(min=12, max=25), 
-                             Regexp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,25}$', 
-                                    message='Password Must Be 12-25 Characters Long And Include At Least One Uppercase Letter, One Lowercase Letter, One Number, And One Special Character.')
+                             Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,25}$", 
+                                    message="Password Must Be 12-25 Characters Long And Include At Least One Uppercase Letter, One Lowercase Letter, One Number, And One Special Character.")
                          ])
-    repeatedPassword = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    biography = StringField('Biography', validators=[Length(max=150)])
-    submit = SubmitField('Register')
+    repeatedPassword = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo("password", message="Passwords Must Match.")])
+    biography = TextAreaField("Biography", validators=[Length(max=150)])
+    submit = SubmitField("Register")
 
-    def validate_email(self, email):
-        blacklist = ['user1@email.com', 'mod1@email.com', 'admin1@email.com']
-        if email.data.lower() in blacklist:
-            raise ValidationError('This Email Is Not Allowed. Please Choose A Different One.')
+    def validateUUsernameEmail(self, usernameEmail):
+        blacklist = ["user1@email.com", "mod1@email.com", "admin1@email.com"]
+        if usernameEmail.data.lower() in blacklist:
+            raise ValidationError("This Email Is Not Allowed. Please Choose A Different One.")
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    rememberMe = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    rememberMe = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+class ChangePasswordForm(FlaskForm):
+    currentPassword = PasswordField("Old Password", validators=[DataRequired()])
+    newPassword = PasswordField("New Password", 
+                         validators=[
+                             DataRequired(), 
+                             Length(min=12, max=25), 
+                             Regexp("^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,25}$", 
+                                    message="Password Must Be 12-25 Characters Long And Include At Least One Uppercase Letter, One Lowercase Letter, One Number, And One Special Character.")
+                         ])
+    confirmNewPassword = PasswordField("Repeat New Password", validators=[DataRequired(), EqualTo("newPassword", message="Passwords Must Match.")])
+    submit = SubmitField("Change Password")
+
+
+    def validateNewPassword(self, newPassword):
+        if newPassword.data == self.currentPassword.data:
+            raise ValidationError("The New Password Must Be Different From The Current Password.")
